830c3831ccb56fb20ae377aff327b4e5514096aa
Christophe Uzureau <uzureau.christophe@gmail.com>
2021-06-14 10:33:15 +0200

    implement linear_conflicts heuristic

--- solver.py
+++ solver.py
@@ -18,35 +18,41 @@ def manhattan_heuristic(state, goal_state, number_of_tiles, size):
     return distance
 
 
-def count_conflicts(row, goal_row, size):
+def count_conflicts(row, goal_row):
     conflicts = 0
 
     for i, (tile, goal_tile) in enumerate(zip(row, goal_row)):
         if tile != goal_tile and tile != 0 and tile in goal_row:
 
             index = goal_row.index(tile)
-            print(f"tile={tile}({i})->({index})")
-            print(f"are anything in {row[index:i]} in {goal_row[i:index]}")
+            # print(f"tile={tile}({i})->({index})")
+            if i > index:
+                # print(f"1- are anything in {row[index:i]} in {goal_row[index + 1:i + 1]}")
+                # for r in row[index:i]:
+                #     if r in goal_row[index + 1:i + 1]:
+                #         print("CONFLICT!")
+                conflicts += len(set(row[i:index]) & set(goal_row[i + 1:index + 1]))
+            else:
+                # print(f"2- are anything in {row[i + 1:index + 1]} in {goal_row[i:index]}")
+                # for r in row[i + 1:index + 1]:
+                #     if r in goal_row[i:index]:
+                #         print("CONFLICT!")
+                conflicts += len(set(row[i + 1:index + 1]) & set(goal_row[i:index]))
 
-            for r in row[index:i]:  #  here inverse index and i
-                if r in goal_row[i:index]: #  here inverse index and i
-                    print("conflicts")
-
-
-    return conflicts
+    return conflicts * 2
 
 
 
 def linear_heuristic(state, goal_state, number_of_tiles, size):
-    distance = 0
+    distance = manhattan_heuristic(state, goal_state, number_of_tiles, size)
 
     for i in range(size):
         # columns
-        distance += count_conflicts(state[i::size], goal_state[i::size], size)
+        distance += count_conflicts(state[i::size], goal_state[i::size])
         # rows
-        distance += count_conflicts(state[i * size:(i + 1) * size], goal_state[i * size:(i + 1) * size], size)
-        print()
+        distance += count_conflicts(state[i * size:(i + 1) * size], goal_state[i * size:(i + 1) * size])
 
+    # print(f"distance={distance}")
     return distance
 
 
@@ -80,7 +86,8 @@ def nextnodes(state, number_of_tiles, size, y):
 
 def search(path, g, threshold, goal_state, number_of_tiles, size, y):
     state = path[0]
-    f = g + manhattan_heuristic(state, goal_state, number_of_tiles, size)
+    # f = g + manhattan_heuristic(state, goal_state, number_of_tiles, size)
+    f = g + linear_heuristic(state, goal_state, number_of_tiles, size)
 
     if f > threshold:
         return f
@@ -108,11 +115,12 @@ def solve(initial_state, goal_state, number_of_tiles, size):
     print(f'INITIAL STATE = {initial_state}')
     print()
 
-    print(linear_heuristic(initial_state, goal_state, number_of_tiles, size))
-    exit()
+    # print(linear_heuristic(initial_state, goal_state, number_of_tiles, size))
+    # exit()
 
     y = initial_state.index(0)
-    threshold = manhattan_heuristic(initial_state, goal_state, number_of_tiles, size)
+    # threshold = manhattan_heuristic(initial_state, goal_state, number_of_tiles, size)
+    threshold = linear_heuristic(initial_state, goal_state, number_of_tiles, size)
     path = deque([initial_state])
 
     while 1:
