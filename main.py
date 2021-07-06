56c2e8f43044ea8d0a56df47d49ebe6d01259747
cuzureau <cuzureau@student.42.fr>
2021-07-06 12:50:08 +0200

    check if puzzle is valid

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import re
 import sys
 from solvability import puzzle_is_solvable
 from solver import solve
 from heuristics import heuristics
-from parser import is_input
+from parser import parser
 
-
-from test2 import linear_conflict_heuristic
 
 def define_goal_state(length):
 	m = [[0] * length for i in range(length)]
@@ -35,33 +32,6 @@ def board_is_valid(board, size):
 		return False
 
 
-def parse_input():
-	try:
-		file = open(sys.argv[1], 'r')
-	except:
-		error('open')
-
-	file = [line.strip().split('#')[0] for line in file]
-	file = [line for line in file if len(line) > 0]
-
-	# print(f"PARSE")
-	# tic = time.time()
-	board = []
-	size = int(file[0][0])		# TODO max size should be 9
-	for fi in file[1:]:
-		fi = re.findall('[0-9]+', fi)
-		for f in fi:
-			board.append(int(f))
-	# print(f'{tuple(board)}\n{(time.time() - tic) * 1000}')
-	# exit()
-
-	# TODO check if board is valid
-	# if board_is_valid(board, puzzle_size) == False:
-	# 	error('invalid')
-
-	return size, tuple(board)
-
-
 def error(name):
 	if name == 'open':
 		print(f"Error:\n\tfile '{sys.argv[1]}' cannot be open")
@@ -73,12 +43,7 @@ def error(name):
 
 
 def main():
-	data, args = is_input()
-	if data:
-		size, initial_state = parse_input(data)
-	else:
-		size = 3
-		initial_state = generate_board(size)
+	initial_state, size, heuristic = parser()
 
 	goal_state = define_goal_state(size)
 	if puzzle_is_solvable(initial_state, goal_state, size):
--- parser.py
+++ parser.py
@@ -1,25 +1,61 @@
 import argparse
 from heuristics import heuristics
 import numpy as np
+import re
 
 
-def generate_board(size):
+
+def generate_state():
+    size = 3
     arr = np.arange(size * size)
     arr2 = np.random.permutation(arr)
 
-    return tuple(arr2)
+    return size, tuple(arr2)
 
 
-def is_input():
-    parser = argparse.ArgumentParser(description='n-puzzle by @cuzureau')
-    parser.add_argument('heuristic', action='store_true', help='heuristic function', default='manhattan')
-    parser.add_argument('file', nargs='?', help='input file', type=argparse.FileType('r'))
+def get_data_from_file(file):
+    data = file.read().splitlines()
+    file.close()
 
-    args = parser.parse_args()
+    data = [line.strip().split('#')[0] for line in data]    # delete comments lines
+    data = [line for line in data if len(line) > 0]         # delete empty lines
+
+    return data
+
+
+def data_is_valid(data):
+    sizes = re.findall('[0-9]+', data[0])
+
+    if len(sizes) == 1:
+        size = int(sizes[0])
+    else:
+        return False
+    if size != len(data[1:]):
+        return False
+    for line in data[1:]:
+        numbers = re.findall('[0-9]+', line)
+        if size != len(numbers):
+            return False
+
+    return True
+
+
+def parser():
+    p = argparse.ArgumentParser(description='n-puzzle by @cuzureau')
+    p.add_argument('-heur', help='heuristic function', choices=list(heuristics.keys()), default='manhattan')
+    p.add_argument('file', help='input file', nargs='?', type=argparse.FileType('r'))
+    args = p.parse_args()
 
     if not args.file:
-        return False
+        size, initial_state = generate_state()
     else:
-        data = args.file.read().splitlines()
-        args.file.close()
-        return data, args
+        data = get_data_from_file(args.file)
+        print(f'Puzzle initial state is {"valid" if data_is_valid(data) else "invalid"}')
+        exit()
+
+        # size, initial_state = define_state(args.file)
+
+    return initial_state, size, args.heur
+
+
+
