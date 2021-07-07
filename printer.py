89ecc2c4d79cbdb1c35ab0ca1689f9ba632b8fcd
cuzureau <cuzureau@student.42.fr>
2021-07-07 11:30:56 +0200

    disaply small update

--- printer.py
+++ printer.py
@@ -1,7 +1,7 @@
 def display(path, node_count):
-    print(f"complexity in time: {node_count}")
-    print(f"complexity in space: {len(path)}")
     print(f"number of moves: {len(path) - 1}")
     print(f"initial state to final state:")
     for p in reversed(path):
         print(f"{p}")
+    print(f"complexity in time: {node_count}")
+    print(f"complexity in space: {len(path)}")
